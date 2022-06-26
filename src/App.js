eb879d9f36d6e5e8cd376f7141e8a34b5d98a522
Enver <enverdavid01@gmail.com>
2022-06-25 19:11:05 -0500

    first migrate

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react';
+import { createGlobalStyle } from 'styled-components';
+import { TeamButtonsContainer } from './components/TeamButtonsContainer';
+
+const GlobalStyle = createGlobalStyle`
+
+  body {
+    background-color: black;
+    font-family: 'Lato', sans-serif;
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+`;
+
+function App() {
+  const [players, setPlayers] = useState([]);
+
+  const getPlayers = async () => {
+    const data = await fetch('https://02-basketball-championship-api-production.up.railway.app/players');
+    // const data = await fetch('http://localhost:4001/players');
+    const players = await data.json();
+    setPlayers(players.data);
+  };
+
+
+  useEffect(() => {
+    getPlayers();
+  }, []);
+
+
+  return (
+    <>
+      <GlobalStyle />
+      <TeamButtonsContainer data={players}/>
+    </>
+  );
+}
+
+export default App;
